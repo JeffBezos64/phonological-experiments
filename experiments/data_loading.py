ccaa7bfe7e8294097db3d3ad5968ea584b6b03da
James Kay <42159491+JeffBezos64@users.noreply.github.com>
2025-04-02 14:03:32 +1300

    change represent what needs to change to process data for other experiments

--- experiments/data_loading.py
+++ experiments/data_loading.py
@@ -7,8 +7,8 @@ def main():
     import pandas 
  
     data_processor = DataProcessor('google/bigbird-roberta-base')
-    data_processor.discover_chunks('/csse/research/NativeLanguageID/mthesis-phonological/experiment/data/balanced/seed_42/non_europe_data')
-
+    data_processor.discover_chunks('/csse/research/NativeLanguageID/mthesis-phonological/experiment/data/balanced/seed_42/europe_data')
+    #this is the same process for both in domain (non_europe) and out of domain (europe)
     #Load the entire dataset as a "test_dataset" since we're going to measure performance with 5FCV
     train_dataset, test_dataset = data_processor.get_train_test_datasets(split_by_chunks=True, seed=42, sequence_length=2048, train_size=0.0)
     test_dataset.extract_encodings_and_labels_from_chunks()
@@ -16,11 +16,11 @@ def main():
     initial = test_dataset.get_dataframe()
     data = initial['text'].to_frame()
     labels = initial['label'].to_frame()
-    with open('/csse/research/NativeLanguageID/mthesis-phonological/experiment/pickles/pickled_datasets/seed_42/full_out_of_domain_experiment_dataframe_clean_chunks.pkl', 'wb') as f:
+    with open('/csse/research/NativeLanguageID/mthesis-phonological/experiment/pickles/pickled_datasets/seed_42/X_europe_out_of_domain_experiment_dataframe_chunks.pkl', 'wb') as f:
         pickle.dump(data, f)
         f.close()
     
-    with open('/csse/research/NativeLanguageID/mthesis-phonological/experiment/pickles/pickled_datasets/seed_42/full_labels_out_of_domain_experiment_dataframe_clean_chunks.pkl', 'wb') as f:
+    with open('/csse/research/NativeLanguageID/mthesis-phonological/experiment/pickles/pickled_datasets/seed_42/y_europe_labels_out_of_domain_experiment_dataframe_chunks.pkl', 'wb') as f:
         pickle.dump(labels, f)
         f.close()
 
